56dd73ba42e9c37df497b3fde721e4e3d1b156c6
Per Lange Laursen <perlangelaursen@gmail.com>
2016-03-04 13:31:18 +0100

    Input handling begun

--- Heureka/Heureka/main.cpp
+++ Heureka/Heureka/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
 
-int main() {
+int main(int argc,char *argv[]) {
+	std::vector<std::string> temp(5);
+	std::string tempString;
+	std::ifstream ifs(argv[1]);
+	if(!(ifs.is_open())) {
+		std::cerr <<  "Error! Could not open file";
+	}
+
+	while (std::getline(ifs, tempString)) {
+		boost::split(temp, tempString, boost::is_any_of(" "));
+	}
+
+	ifs.close();
+
 	std::cout << "Hello World\n";
 	return 0;
 }
