48020e656e87ae63618b4b725018e0f875d28f20
Per Lange Laursen <perlangelaursen@gmail.com>
2016-03-09 10:56:25 +0100

    Added a Comparator class for usage with the Priority Queue

---
+++ Heureka/Heureka/Comparator.h
@@ -0,0 +1,22 @@
+//
+// Created by Per Lange Laursen on 09/03/16.
+//
+
+#ifndef INC_02180_HEUREKA_COMPARATOR_H
+#define INC_02180_HEUREKA_COMPARATOR_H
+
+#include "State.h"
+
+namespace Heureka {
+    class Comparator;
+}
+
+class Comparator {
+public:
+    int operator() (const Heureka::State s1, const Heureka::State s2) {
+        return s1.distance > s2.distance;
+    }
+};
+
+
+#endif //INC_02180_HEUREKA_COMPARATOR_H
