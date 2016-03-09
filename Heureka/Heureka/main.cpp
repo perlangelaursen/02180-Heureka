a673a273ee5dcdac0a6b452e7ebdc090d72debac
Per Lange Laursen <perlangelaursen@gmail.com>
2016-03-09 10:47:22 +0100

    Fixed main.cpp

--- Heureka/Heureka/main.cpp
+++ Heureka/Heureka/main.cpp
@@ -3,53 +3,25 @@
 #include <vector>
 #include <sstream>
 
-void readFromFile(std::string file);
-
-int main(int argc, char *argv[]) {
-	std::string file = argv[1];
-	//readFromFile("Copenhagen.txt");
+int main(int argc,char *argv[]) {
 	std::vector<std::string> temp(5);
 	std::string tempString;
-	std::ifstream ifs(file);
-	std::istringstream sstream(tempString);
-	int n = 0;
+	std::ifstream ifs(argv[1]);
+	std::istringstream istringstream(tempString);
+	int n=0;
 	if(!(ifs.is_open())) {
-		std::cerr << "Error! Could not open file";
+		std::cerr <<  "Error! Could not open file";
 	}
 
-	while (getline(ifs, tempString)) {
-		for (std::istream_iterator<std::string> iterator = std::istream_iterator<std::string>(sstream);
+	while (std::getline(ifs, tempString)) {
+		for (std::istream_iterator<std::string> iterator = std::istream_iterator<std::string>(istringstream);
 			 iterator != std::istream_iterator<std::string>() && n < 5; ++iterator, ++n) {
 			temp[n] = *iterator;
 		}
-
-
 	}
 
 	ifs.close();
 
 	std::cout << "Hello World\n";
-	return 0;
-}
-
-void readFromFile(std::string file) {
-	std::vector<std::string> temp(5);
-	std::string tempString;
-	std::ifstream ifs(file);
-	std::istringstream sstream(tempString);
-	int n = 0;
-	if(!(ifs.is_open())) {
-		std::cerr << "Error! Could not open file";
-	}
-
-	while (getline(ifs, tempString)) {
-		for (std::istream_iterator<std::string> iterator = std::istream_iterator<std::string>(sstream);
-			 iterator != std::istream_iterator<std::string>() && n < 5; ++iterator, ++n) {
-			temp[n] = *iterator;
-		}
-
-
-	}
-
-	ifs.close();
+		return 0;
 }
