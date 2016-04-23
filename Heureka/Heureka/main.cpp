919d82f0126631e1e69841eccf1ad5d98d059593
Per Lange Laursen <perlangelaursen@gmail.com>
2016-04-23 18:14:29 +0200

    Added argument selection to main.cpp

--- Heureka/Heureka/main.cpp
+++ Heureka/Heureka/main.cpp
@@ -4,11 +4,23 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
 	PathfindingDriver driver;
-	driver.run();
-
 	InferenceDriver inferenceDriver;
-	inferenceDriver.run();
+	if(argc == 2) {
+		if(strcmp(argv[1], "pathfinding") == 0) {
+			driver.run();
+		} else if (strcmp(argv[1], "inference") == 0) {
+			inferenceDriver.run();
+		} else {
+			std::cout << "Please specify arguments\n";
+			std::cout << "\t 1. pathfinding\n";
+			std::cout << "\t 2. inference";
+		}
+	} else {
+		driver.run();
+		inferenceDriver.run();
+	}
+
 	return 0;
 }
